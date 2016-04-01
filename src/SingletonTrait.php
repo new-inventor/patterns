a8ca525ee2214669c3c3019ecb08f6d19c4a9ce1
Ionov George <g.ionov@php73.ru>
2016-04-01 17:48:14 +0400

    get instance new mechanics

--- src/SingletonTrait.php
+++ src/SingletonTrait.php
@@ -14,45 +14,23 @@ trait SingletonTrait
     }
 
     /**
-     * @param string $initMethodName
      * @return static
      * @throws \Exception
      */
-    public static function getInstance($initMethodName = '__construct')
+    public static function getInstance()
     {
-        if(!is_string($initMethodName)){
-            throw new \Exception('Parameter "initMethodName" should be a string.');
-        }
-
-        if(null === self::$instance){
-            $args = func_get_args();
-            array_shift($args);
+        $args = func_get_args();
+        if(null === self::$instance || !empty($args)){
             $calledClass = get_called_class();
             $reflection = new \ReflectionClass($calledClass);
             $instance = $reflection->newInstanceWithoutConstructor();
-            self::getMethod($reflection, $initMethodName)->invokeArgs($instance, $args);
+            $constructor = $reflection->getConstructor();
+            $constructor->setAccessible(true);
+            $constructor->invokeArgs($instance, $args);
             self::$instance = $instance;
         }
 
         return self::$instance;
-    }
-
-    /**
-     * @param \ReflectionClass $reflection
-     * @param string $methodName
-     * @return \ReflectionMethod
-     * @throws \Exception
-     */
-    protected static function getMethod(\ReflectionClass $reflection, $methodName = '__construct')
-    {
-        if($reflection->hasMethod($methodName)){
-            $method = $reflection->getMethod($methodName);
-            $method->setAccessible(true);
-
-            return $method;
-        }else{
-            throw new \Exception('Method "' . $methodName . '" does not exists in class "' . $reflection->getName() . '".');
-        }
     }
 
     /**
